bb0ba60fff04fd76a1e1b8ff81742deb44ca282b
Иван <folmer.ivan@mail.ru>
2023-11-11 08:55:36 +0300

    My first commit to PythonAPI

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import requests
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text)
+
+payload = {"login": "secret_login", "password": "secret_pass2"}
+response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
+
+cookie_value = response1.cookies.get('auth_cookie')
+
+cookies = {}
+if cookie_value is not None:
+    cookies.update({'auth_cookie': cookie_value})
+
+response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookies)
+
+print(response2.text)
