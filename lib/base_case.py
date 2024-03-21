e1c35f601ebee7b4b854df055a878a7c60638b15
ivan.folmer <folmer.ivan@mail.ru>
2024-03-21 16:50:18 +0300

    Commit at 21/03/2024

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,6 +1,4 @@
-import json.decoder
-
-
+from requests import Response
 class BaseCase:
     def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
--- tests/Auth_user_poss_api.py
+++ tests/Auth_user_poss_api.py
@@ -2,8 +2,9 @@ from typing import Dict
 
 import pytest
 import requests
+from lib.base_case import BaseCase
 
-class TestUserAuth:
+class TestUserAuth(BaseCase):
     def test_auth_user(self):
         data = {
             'email':'rmtest@krit.pro',
@@ -11,13 +12,9 @@ class TestUserAuth:
         }
         response1=requests.post("https://riskhunter.krit.pro/api/v1/auth/SignIn", data=data)
 
-        assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
-        assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response"
-        assert "user_id" in response1.json(), "There is no user id in the response"
 
-        auth_sid = response1.cookies.get("auth_sid")
-        token=response1.headers.get("x-csrf-token")
-        user_id_from_auth_method=response1.json()["user_id"]
+        assert "user_id" in response1.json(), "There is no user id in the response"
+        self.user_id_from_auth_method=response1.json()["user_id"]
 
         response2=requests.get(
             "https://riskhunter.krit.pro/api/v1/users/me",
