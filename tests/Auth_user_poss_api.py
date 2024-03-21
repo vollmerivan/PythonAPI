bc4c18516d2da587a04b236ba008bfbcaffd5cd7
ivan.folmer <folmer.ivan@mail.ru>
2024-03-21 17:06:20 +0300

    Commit at 21/03/2024

--- tests/Auth_user_poss_api.py
+++ tests/Auth_user_poss_api.py
@@ -5,61 +5,47 @@ import requests
 from lib.base_case import BaseCase
 
 class TestUserAuth(BaseCase):
-    def test_auth_user(self):
+    exclude_params = [
+        ("no_cookie"),
+        ("no_token")
+    ]
+    def setup(self):
         data = {
             'email':'rmtest@krit.pro',
             'password':'super'
         }
         response1=requests.post("https://riskhunter.krit.pro/api/v1/auth/SignIn", data=data)
 
-
-        assert "user_id" in response1.json(), "There is no user id in the response"
-        self.user_id_from_auth_method=response1.json()["user_id"]
-
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
+    def test_auth_user(self):
         response2=requests.get(
             "https://riskhunter.krit.pro/api/v1/users/me",
-            headers={"x-csrf-token":token},
-            cookies={"auth_sid":auth_sid}
+            headers={"x-csrf-token":self.token},
+            cookies={"auth_sid":self.auth_sid}
         )
 
         assert "user_id" in response2.json(), "There is no user id in the response"
 
         user_id_from_check_method = response2.json()["user_id"]
 
-        assert user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal to user id from check method"
-
-    exclude_params = [
-            ("no_cookie"),
-            ("no_token")
-    ]
+        assert self.user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal to user id from check method"
 
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negativ_auth_check(self, condition):
-        data = {
-            'email': 'rmtest@krit.pro',
-            'password': 'super'
-        }
-        response1 = requests.post("https://riskhunter.krit.pro/sign-in", data=data)
-
-        assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
-        assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response"
-        assert "user_id" in response1.json(), "There is no user id in the response"
-
-        auth_sid = response1.cookies.get("auth_sid")
-        token = response1.headers.get("x-csrf-token")
-
         if condition == "no_cookie":
             response2 = requests.get(
                 "https://riskhunter.krit.pro/sign-in",
-                headers={"x-csrf-token":token}
+                headers={"x-csrf-token":self.token}
             )
         else:
             response2=requests.get(
                 "https://riskhunter.krit.pro/sign-in",
-                cookies={"auth_sid":auth_sid}
+                cookies={"auth_sid":self.auth_sid}
             )
 
-        assert "user_id" in response1.json(), "There is no user id in the second response"
+        assert "user_id" in self.response2.json(), "There is no user id in the second response"
 
         user_id_from_check_method = response2.json()["user_id"]
 
